89243638824e45f03dac1bef8b5062e0ddac633b
Vilho Mäkeläinen <vilho.makelainen@tuni.fi>
2023-11-23 02:30:50 +0200

    added comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
+#include <iostream> // import iostream
 
 int main() {
-
-    std::cout << "Hello world!" << std::endl;
-
-    return 0;
+    std::cout << "Hello world!" << std::endl; // Write to std out
+    return 0; // return success
 }
